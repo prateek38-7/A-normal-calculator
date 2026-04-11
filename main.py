b0ea3909934af3d403566579bad5dd44de12f73e
prateek38-7 <prateekals0@gmail.com>
2026-04-11 16:41:34 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+a = int(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+b = int(input("Enter second number: "))
+
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+elif op == "/":
+    print(a / b)
+else:
+    print("Invalid operator")
