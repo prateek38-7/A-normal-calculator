40b2682a5ee02481a715b8114b9b28001cb823da
prateek38-7 <prateekals0@gmail.com>
2026-04-27 13:24:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-a = int(input("Enter first number: "))
-op = input("Enter operator (+, -, *, /): ")
-b = int(input("Enter second number: "))
 
-if op == "+":
-    print(a + b)
-elif op == "-":
-    print(a - b)
-elif op == "*":
-    print(a * b)
-elif op == "/":
-    print(a / b)
-else:
-    print("Invalid operator") 
+while True:
+ a = int(input("Enter first number: "))
+ op = input("Enter operator (+, -, *, /): ")
+ b = int(input("Enter second number: "))
+
+ if op == "+":
+     print(a + b)
+ elif op == "-":
+     print(a - b)
+ elif op == "*":
+     print(a * b)
+ elif op == "/":
+     print(a / b) 
+ else:
+     print("Invalid operator") 
 
 # this is my first project 
